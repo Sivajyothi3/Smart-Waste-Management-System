884d126bb1f2048fa80158552c262f2a8aa2113c
V.Siva Jyothi <varikuntasj@gmail.com>
2025-11-05 08:56:02 +0530

    Updated main class to use cloud properties

--- src/main/java/com/smartwaste/SmartWasteSystemApplication.java
+++ src/main/java/com/smartwaste/SmartWasteSystemApplication.java
@@ -2,9 +2,12 @@ package com.smartwaste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-cloud.properties")  // 👈 tells Spring Boot to use cloud properties
 public class SmartWasteSystemApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SmartWasteSystemApplication.class, args);
     }
